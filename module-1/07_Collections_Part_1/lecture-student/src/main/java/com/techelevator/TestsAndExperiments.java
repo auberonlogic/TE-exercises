ba4054808ecb2daa165ee82a9c6ed6d705ec23d4
YOUR NAME GOES HERE <rwtheerman@gmail.com>
2021-09-22 11:58:26 -0400

    Day 8 lecture notes

--- module-1/07_Collections_Part_1/lecture-student/src/main/java/com/techelevator/TestsAndExperiments.java
+++ module-1/07_Collections_Part_1/lecture-student/src/main/java/com/techelevator/TestsAndExperiments.java
@@ -18,29 +18,32 @@ public class TestsAndExperiments {
         strings.add("F");
         strings.add("G");
 
+        System.out.println("Oringal List: ");
         System.out.println(strings);
         System.out.println("List size is: " + strings.size() + "\n");
 
         // add an elements to the end of the list
         strings.add("0"); // if index is not specified, element will be appended to the end of the list
-
+        System.out.println("Add \"0\" to end of list: ");
         System.out.println(strings);
         System.out.println("List size is: " + strings.size() + "\n");
 
         // insert an element at a given index
         strings.add(3, "777"); // should insert "777" at the 3rd index, as the 4th element
-
+        System.out.println("Insert \"777\" at 3rd index: ");
         System.out.println(strings);
         System.out.println("List size is: " + strings.size() + "\n");
 
         // remove and element at a given index
         strings.remove(7); // should remove "G" and reduce size by 1
+        System.out.println("Remove \"G\" from 7th index: ");
         System.out.println(strings);
         System.out.println("List size is: " + strings.size() + "\n");
 
         // replace an element at a given index
         strings.remove(0);
         strings.add(0, "999");
+        System.out.println("Remove \"A\" from 0th index and replace it with \"999\": ");
         System.out.println(strings);
         System.out.println("List size is: " + strings.size() + "\n");
     }
