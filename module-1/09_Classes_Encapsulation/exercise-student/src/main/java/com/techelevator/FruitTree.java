000861366f3b9f54c422c04eca3a78ea8666428e
Tom Medvitz <tom@techelevator.com>
2021-09-23 12:26:57 -0400

    New material
    (cherry picked from commit e9948fd01fb172ec59b09461a005aacf1c889425)

--- module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/FruitTree.java
+++ module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/FruitTree.java
@@ -2,33 +2,4 @@ package com.techelevator;
 
 public class FruitTree {
 
-    // Instance variables
-    private String typeOfFruit;
-    private int piecesOfFruitLeft;
-
-    // Constructor
-    public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
-        this.typeOfFruit = typeOfFruit;
-        this.piecesOfFruitLeft = startingPiecesOfFruit;
-    }
-
-    // pickFruit method
-    public boolean pickFruit(int numberOfPiecesToRemove) {
-        if (piecesOfFruitLeft >= numberOfPiecesToRemove) {
-            piecesOfFruitLeft -= numberOfPiecesToRemove;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // Getters
-    public String getTypeOfFruit() {
-        return typeOfFruit;
-    }
-
-    public int getPiecesOfFruitLeft() {
-        return piecesOfFruitLeft;
-    }
-
 }
