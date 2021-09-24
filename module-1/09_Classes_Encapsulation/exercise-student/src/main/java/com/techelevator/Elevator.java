ec338da8aaeb21ee372ba27a8e0aca409b3194c3
YOUR NAME GOES HERE <rwtheerman@gmail.com>
2021-09-23 22:37:29 -0400

    day 9 homework done

--- module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/Elevator.java
+++ module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/Elevator.java
@@ -2,4 +2,57 @@ package com.techelevator;
 
 public class Elevator {
 
+    // instance variables
+    private int currentFloor;
+    private int numberOfFloors;
+    private boolean doorOpen;
+
+    // Constructor
+    public Elevator(int numberOfLevels) {
+        this.numberOfFloors = numberOfLevels;
+        currentFloor = 1;
+    }
+
+    // Methods
+    public void openDoor() {
+        doorOpen = true;
+    }
+
+    public void closeDoor() {
+        doorOpen = false;
+
+    }
+
+    public void goUp(int desiredFloor) {
+        if (currentFloor < desiredFloor) {
+            if (!doorOpen) {
+                if (desiredFloor <= numberOfFloors) {
+                    currentFloor = desiredFloor;
+                }
+            }
+        }
+    }
+
+    public void goDown(int desiredFloor) {
+        if (currentFloor > desiredFloor) {
+            if (!doorOpen) {
+                if (desiredFloor > 0) {
+                    currentFloor = desiredFloor;
+                }
+            }
+        }
+    }
+
+    // Getters
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public boolean isDoorOpen() {
+        return doorOpen;
+    }
 }
