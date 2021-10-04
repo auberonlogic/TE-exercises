8c66689c789533a08eac083737347e893d146036
Tom Medvitz <tom@techelevator.com>
2021-10-04 12:22:42 -0400

    New material
    (cherry picked from commit 49e922295b66505a6898ace1d253bc848b08246a)

--- module-1/14_Unit_Testing/exercise-student/src/test/java/com/techelevator/WordCountTest.java
+++ module-1/14_Unit_Testing/exercise-student/src/test/java/com/techelevator/WordCountTest.java
@@ -3,59 +3,19 @@ package com.techelevator;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WordCountTest {
-
-//    getCount(["ba", "ba", "black", "sheep"]) → {"ba" : 2, "black": 1, "sheep": 1 }
-//    getCount(["a", "b", "a", "c", "b"]) → {"a": 2, "b": 2, "c": 1}
-//	  getCount([]) → {}
-//	  getCount(["c", "b", "a"]) → {"c": 1, "b": 1, "a": 1}
-
-    @Test
-    public void returns_correct_count_when_all_words_are_different() {
-        WordCount wc = new WordCount();
-        String[] input = {"c", "b", "a"};
-        Map<String, Integer> output = new HashMap<>();
-        output.put("c", 1);
-        output.put("b", 1);
-        output.put("a", 1);
-        Assert.assertEquals(output, wc.getCount(input));
-    }
-
-    @Test
-    public void returns_empty_map_when_input_array_is_empty() {
-        WordCount wc = new WordCount();
-        String[] input = new String[]{};
-        Map<String, Integer> output = new HashMap<>();
-        Assert.assertEquals(output, wc.getCount(input));
-    }
-
-    @Test
-    public void returns_correct_count_when_some_words_are_the_same() {
-        WordCount wc = new WordCount();
-        String[] input = {"ba", "ba", "black", "sheep"};
-        Map<String, Integer> output = new HashMap<>();
-        output.put("ba", 2);
-        output.put("black", 1);
-        output.put("sheep", 1);
-        Assert.assertEquals(output, wc.getCount(input));
-    }
-
-    @Test
-    public void null_input_returns_null_output() {
-
-        WordCount wc = new WordCount();
-        Assert.assertNotNull(wc);
-    }
 
     @Test
     public void please_work() {
         // Arrange
         WordCount wc = new WordCount();
         String[] input = "ba ba black sheep".split(" ");
+        String rejoined = String.join(",", input);
+
+        System.out.println("Rejoined: " + rejoined);
         Map<String, Integer> expected = new HashMap<>();
         expected.put("ba", 2);
         expected.put("black", 1);
@@ -82,14 +42,27 @@ public class WordCountTest {
     }
 
     @Test
-    public void empty_String_Array_input() {
+    public void empty_array_input() {
         WordCount wc = new WordCount();
         String[] input = new String[0];
-        Map<String, Integer> output = wc.getCount(input);
         Map<String, Integer> expected = new HashMap<>();
 
-        Assert.assertEquals(expected, output);
+        Map<String, Integer> output = wc.getCount(input);
 
+        Assert.assertNotNull(output);
+        Assert.assertEquals(expected, output);
     }
 
+    @Test
+    public void input_array_with_one_element() {
+        WordCount wc = new WordCount();
+        String[] input = new String[] {"Hello"};
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("Hello", 1);
+
+        Map<String, Integer> output = wc.getCount(input);
+
+        Assert.assertNotNull(output);
+        Assert.assertEquals(expected, output);
+    }
 }
