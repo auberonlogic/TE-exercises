14af3a8f63098b3675ebaa97c48814a746bc9366
Tom Medvitz <tom@techelevator.com>
2021-10-14 12:37:03 -0400

    New material
    (cherry picked from commit cdaecac52b650ebf240ebc3d3b748dcdf5f4935c)

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/Exercises/11_add_director.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise-student/Exercises/11_add_director.sql
@@ -1,5 +1,2 @@
 -- 11. Hollywood is remaking the classic movie "The Blob" and doesn't have a director yet. Add yourself to the person table, and assign yourself as the director of "The Blob" (the movie is already in the movie table). (1 record each)
-INSERT INTO person  VALUES (DEFAULT, 'Ryan Theerman', '1987-12-15', null, 'Ryan was born in a state of ignorance and has been trying to fix that ever since.', null, null);
-UPDATE movie
-SET director_id = (SELECT person_id FROM person WHERE person_name = 'Ryan Theerman')
-WHERE title = 'The Blob';
+
