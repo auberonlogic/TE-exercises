5f4069b57724e037c960062dfeeb56638c8c78e6
YOUR NAME GOES HERE <rwtheerman@gmail.com>
2021-10-22 09:06:35 -0400

    misc edits

--- module-2/08_Integration_Testing/exercise-student/src/main/java/com/techelevator/projects/dao/JdbcTimesheetDao.java
+++ module-2/08_Integration_Testing/exercise-student/src/main/java/com/techelevator/projects/dao/JdbcTimesheetDao.java
@@ -37,7 +37,7 @@ public class JdbcTimesheetDao implements TimesheetDao {
                      "WHERE employee_id = ? " +
                      "ORDER BY timesheet_id;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, employeeId);
-        while (results.next()) {
+        while(results.next()) {
             Timesheet timesheet = mapRowToTimesheet(results);
             timesheets.add(timesheet);
         }
