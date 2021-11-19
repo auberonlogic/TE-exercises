21d8cc530887dd0d531a98ebee6f1baeb213e308
YOUR NAME GOES HERE <rwtheerman@gmail.com>
2021-11-19 12:25:24 -0500

    03-10 lecture notes

--- module-3/10_Review/lecture-student/hero/hero.js
+++ module-3/10_Review/lecture-student/hero/hero.js
@@ -1,2 +1,34 @@
 const nerdyURL = 'http://api.icndb.com/jokes/random?exclude=explicit&limitTo=nerdy';
 const randomURL = 'http://api.icndb.com/jokes/random?exclude=explicit';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nerdy = document.getElementById('nerdButton');
+    const h4 = document.querySelector('h4');
+    nerdy.addEventListener('click', () => {
+        
+        fetch(nerdyUrl).then((response) => {
+            return response.json();
+        }).then((data) => {
+            // Do your DOM manipulation here
+            
+            h4.innerText = data.value.joke;
+        }); 
+    });
+
+    const rando = document.getElementById('randomButton');
+    rando.addEventListener('click', () => {
+        fetch(randomURL)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            h4.innerText = data.value.joke;
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+    });
+
+
+});
